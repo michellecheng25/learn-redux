6f97332b02dc27d01f691809e50bc454a15fcc92
Michelle Cheng <michellecheng.2017@gmail.com>
2022-04-22 16:55:40 -0400

    createSlice

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import configureStore from "./store/configureStore";
 import * as actions from "./store/bugs";
+import { projectAdded } from "./store/projects";
 
 const store = configureStore();
 
@@ -12,5 +13,6 @@ store.dispatch(actions.bugAdded("bug 1!"));
 store.dispatch(actions.bugResolved(1));
 */
 
+store.dispatch(projectAdded({ name: "Project 1" }));
 store.dispatch(actions.bugAdded({ description: "Bug 1" }));
 store.dispatch(actions.bugResolved({ id: 1 }));
